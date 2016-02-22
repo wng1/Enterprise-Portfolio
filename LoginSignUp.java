0b76665287c7e6ce771eefb0c35c133047b235a8
wng1 <wng1@users.noreply.github.com>
2016-02-22 16:05:20 +0000

    Update LoginSignUp.java

--- LoginSignUp.java
+++ LoginSignUp.java
@@ -3,5 +3,12 @@ public class LoginSignup extend HttpServlet
 
 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException SerletException
 {
+    processLoginReg(request, response)
+}
 
+protected processLoginReg(HttpServletRequest request, HttpServlet response)
+{
+  response.setHeader("content-type", "application/json;charset=utf-8");
+  //response.setContentType("text/html");
+  //response.setHeader("context-type", "text/html");
 }
