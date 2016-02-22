45268f28023a45fbf987869d97c61f70a0ffbf72
wng1 <wng1@users.noreply.github.com>
2016-02-22 16:22:22 +0000

    Update LoginSignUp.java

--- LoginSignUp.java
+++ LoginSignUp.java
@@ -14,4 +14,15 @@ protected processLoginReg(HttpServletRequest request, HttpServlet response)
   //File extension and browser, OS will detect
   
   PrintWriter out = response.getWriter();
+  Result loginRegResponse = createLoginReg(request);
+  
+  String username = request.getParameter("username");
+  String password = request.getParameter("password");
+  
+  //In other file it retrives the parameters 
+  //<form name="loginReg" action="login" method="post">
+  //<input type="text" name="username">
+  //<input type="password" name="password">
+  //<input type="submit" value="login">
+  //</form>   Simple html form input, can be validated with UI graphics, jquery designs, error handling
 }
