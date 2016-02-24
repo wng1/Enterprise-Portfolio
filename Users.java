f55ef119b7e17417259fefe573c4ae555f6ac187
wng1 <wng1@users.noreply.github.com>
2016-02-24 23:54:31 +0000

    Create Users.java

---
+++ Users.java
@@ -0,0 +1,8 @@
+public class EmployeeServlet extends HttpServlet
+{
+protected final String databaseURL="jdbc:mysql:myDatabase";
+protected final String tableName = "Users";
+protected final String username = "";
+protected final String password = "";
+
+}
