2e6b351219f0025b8d380cd333b3b2b7343ee6fd
wng1 <wng1@users.noreply.github.com>
2016-02-25 12:43:46 +0000

    Create NameController.java

---
+++ NameController.java
@@ -0,0 +1,9 @@
+import javax.faces.bean.*;
+
+@ManagedBean
+
+public class NameController
+{
+private String firstName="", lastName="";
+//This intialities bi values in the parameters of the form
+}
