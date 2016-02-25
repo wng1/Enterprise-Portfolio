5fd27200f039844330f1e181afa6d062a33e6163
wng1 <wng1@users.noreply.github.com>
2016-02-25 01:59:34 +0000

    Update Users.java

--- Users.java
+++ Users.java
@@ -13,4 +13,37 @@ public void doGet(HttpServletRequest request, HttpServletResponse response)
         //optional to include the HTML format for good practice
         showTable(out);
       }
+      
+//next is to make an connection
+
+protected void showTable(PrintWriter out)
+{
+      try
+      {
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            String query = "SELECT * FROM" + tableName; //To be changed
+            
+            ResultSet rs = stmt.executeQuery(query);
+            printTableTop(conn, rs, out);
+            printTableBody(rs, out);
+            conn.close();
+      }
+      catch (Exception e)
+      {
+            System.err.println("Error:" +e); //to be changed as well
+      }
+}
+
+protected Connection getConnection() throw Exception
+{
+//Establish network connection to database
+Properties userInfo = new Properties():
+userInfo.put("user". username);
+userInfo.put("password", password);
+Connection conn = DriverManager.getConnection(url, userInfo);
+return (conn);
+}
+ 
+
 }
