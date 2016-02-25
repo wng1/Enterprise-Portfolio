7a34cac0d627521d9f614ab5a485f2efe7e93d19
wng1 <wng1@users.noreply.github.com>
2016-02-25 12:52:55 +0000

    Update NameController.java

--- NameController.java
+++ NameController.java
@@ -4,6 +4,28 @@ import javax.faces.bean.*;
 
 public class NameController
 {
-private String firstName="", lastName="";
+  private String firstName="", lastName="";
 //This intialities bi values in the parameters of the form
+
+  public String getFirstName() 
+  {
+    return(firstName);
+  }
+  
+  public void setFirstName(String firstName) 
+  {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() 
+  {
+    return(lastName);
+  }
+
+  public void setLastName(String lastName) 
+  {
+    this.lastName = lastName;
+  }
+  
+  
 }
