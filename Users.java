46609563c7ae2bc4a0a2d2ab476f96372ccbac5f
wng1 <wng1@users.noreply.github.com>
2016-02-25 01:01:00 +0000

    Update Users.java

--- Users.java
+++ Users.java
@@ -5,4 +5,12 @@ protected final String tableName = "Users";
 protected final String username = "";
 protected final String password = "";
 
+public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws ServletException, IOException
+      {
+        response.setContentType("text/html")
+        PrintWriter out = response.getWriter();
+        //optional to include the HTML format for good practice
+        showTable(out);
+      }
 }
