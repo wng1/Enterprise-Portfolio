6f11b6551f419222fceeb087b66972fca93fc1b2
wng1 <wng1@users.noreply.github.com>
2016-02-26 23:42:08 +0000

    Update Account.java

--- Account.java
+++ Account.java
@@ -6,6 +6,7 @@ public class AccountBean
 {
 protected String UserID, Username, Password;
 protected Users user;
+private static UserCheck checkUser = new UserMap();
 
 public String getUserID()
 {
@@ -39,10 +40,21 @@ public User getUser()
 
 public String showAccountInformation()
 {
-  if(!password.equals("test"))
+  if(!password.equals("test"))   //check userid validity
   {
     return("incorrect-pass");   //Next version, improved security and crytography
   }
+  
+  if (user == null) { return("no-User");
+  else if(user.getSubscriptionPlan() == "basic")
+  {
+    return ("basic")
+  } else if (user.getSubscriptionPlan() == "premium")
+  {
+    return ("premium")
+  }
+  else { return ("advanced")  };
+  
 }
 
 }
