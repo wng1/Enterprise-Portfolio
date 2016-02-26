13b840f33be4beb71c8bb047b3ab636c5d6cdf38
wng1 <wng1@users.noreply.github.com>
2016-02-26 22:58:05 +0000

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,15 @@
+import javax.faces.bean.*;
+
+@ManagedBean //Java Bean managed by JSF framework
+
+public class AccountBean
+{
+protected String UserID, Username, Password;
+protected Users user;
+
+public String gerUserID()
+{
+
+}
+
+}
