f5099c5bafd3b18fb9d00b0a6a8e913ab7dca7c5
wng1 <wng1@users.noreply.github.com>
2016-03-01 15:10:24 +0000

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,22 @@
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xmlns:h="http://xmlns.jcp.org/jsf/html">
+      
+<h:head>
+<title>Order - Step 1 -3</Step)
+<link href="./css/style.css" ref="stylesheet" type="text/css"/>
+</h:head>
+
+<h:body>
+
+
+<h1 class="title">Place your order</h1>
+
+<ul>
+<li>Status: #{OrderBean.currentStatus}</li>
+<li>Order ID: #{OrderBean.orderID}</li>
+</ul>
+
+</h:body>
+
+</html>
+
