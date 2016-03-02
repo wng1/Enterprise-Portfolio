d262afa79f3bee0f3988ba29a5587b8e0e8bef2d
wng1 <wng1@users.noreply.github.com>
2016-03-02 19:07:06 +0000

    Update Registration.java

--- Registration.java
+++ Registration.java
@@ -1,1 +1,26 @@
 
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Registration extend HttpServlet 
+{
+  public void doGet(HttpServletRequest rq, HttpServletResponse rsp) throws ServletException, IOException
+  {
+    response.setContentType("text.html");
+    PrintWriter output = response.getWriter(); //utilises the java.io
+    
+    String actionURL="/servlet/RegistrationServlet";
+    String firstName = CookieUtilites.getCookieValue(request, "firstName", "");
+    
+    //Repeat for the other relevant fields
+     //Basic outline
+  <HTML>
+  <BODY>
+   <FORM ACTION=\""+actionURL + "\">\n" + "First Name:" +
+   <INPUT TYPE=\"TEXT\" NAME=\"firstName\" " +"VALUE=\" "+FirstName + "\">
+   <INPUT TYPE=\"SUBMIT\" VALUE=\"REGISTER"></FORM>
+   </BODY>
+   </HTML>
+  }
+}
