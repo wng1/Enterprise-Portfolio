a13065ccc8c283ed2d3c7b227684adafcb7d3113
wng1 <wng1@users.noreply.github.com>
2016-03-04 18:46:11 +0000

    Update ProductPage.java

--- ProductPage.java
+++ ProductPage.java
@@ -14,7 +14,26 @@ public abstract class ProductPage extends HttpServlet
   for(int i=0; i<products.length; i++)
   products[i] = Product.getProduct(pID[i]);
   //Create a new arraylist with product ID in the array list, then run through this arraylist of the indexes
-  
 }
+  //Display the content using the doGet Method
+  
+  protected void doGet(HttpServletRequest request, HttpSerrvletResponse response) throws IOException, ServletException
+  {
+    if (products == null)
+    {
+      response.sendError(response, "Missing products");
+      //public void sendError(int code, String message) 
+      This sendError method sends a status code with short message formatted inside an HTML file and sent to the client.
+      return;
+    }
+    response.setContentType("text/html");
+    PrintWriter output = response.getWriter();
+    
+    String outputFile = ""
+    // HTML coding
+    
+  }
+  
+  
   
 }
