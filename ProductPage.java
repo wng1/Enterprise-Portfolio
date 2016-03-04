4825d0c89a4c7af9cb7ca829ce35c06e6fea6ade
wng1 <wng1@users.noreply.github.com>
2016-03-04 18:26:33 +0000

    Update ProductPage.java

--- ProductPage.java
+++ ProductPage.java
@@ -4,4 +4,17 @@ public abstract class ProductPage extends HttpServlet
   private String[] pID;
   private String title;
   
+  //When using servlet, this method should be called before the servlet is accessed. 
+  
+  protected void setProducts(String[] pID)
+{
+  this.pID = pID;
+  products = new ProductItems[pID.length];
+  
+  for(int i=0; i<products.length; i++)
+  products[i] = Product.getProduct(pID[i]);
+  //Create a new arraylist with product ID in the array list, then run through this arraylist of the indexes
+  
+}
+  
 }
