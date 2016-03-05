3176af77908506fe51c511ff5f4cc3c616fbc4e2
wng1 <wng1@users.noreply.github.com>
2016-03-05 17:11:23 +0000

    Create Cart.java

---
+++ Cart.java
@@ -0,0 +1,15 @@
+public class Cart
+{
+private ArrayList listOfProducts;
+
+public Cart()
+{
+listOfProducts = new ArrayList();
+}
+
+public List getListofProducts()
+[
+return(listOfProducts);
+}
+
+}
