37adf207451e6ad8f9cf0370a795904d4a92a752
wng1 <wng1@users.noreply.github.com>
2016-03-05 14:12:16 +0000

    Create OrderDetails.java

---
+++ OrderDetails.java
@@ -0,0 +1,12 @@
+public class OrderDetails
+{
+private Products product;
+private int numOfProducts;
+
+public OrderDetails(Products product)
+{
+setProduct(product);
+setNumOfProducts(1); //This will be on default
+}
+
+}
