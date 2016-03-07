e8eebd6192499718c78e4d3c3c6090301e5ce730
wng1 <wng1@users.noreply.github.com>
2016-03-07 03:19:08 +0000

    Create AccountBean.java

---
+++ Beans/AccountBean.java
@@ -0,0 +1,25 @@
+pacakage com.wng;
+
+public class Account
+{
+
+public setTest1(String AccountID)
+{
+this.test1= AccountID;
+}
+
+public setTest2(String AccountName)
+{
+this.test2= AccountName;
+}
+
+public getTest1()
+{
+System.out.println("Account ID:" + test1);
+}
+public getTest2)
+{
+System.out.println("Account Name:" + test2);
+}
+
+}
