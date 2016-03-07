8b016994e8249da9bd979b3d61ad84d134cbce79
wng1 <wng1@users.noreply.github.com>
2016-03-07 18:49:31 +0000

    Update crypto.java

--- Security/crypto.java
+++ Security/crypto.java
@@ -1,6 +1,8 @@
 import java.security.SecureRandom;
 import sun.misc.BASE64Encoder;
 
+
+//Use JRE 1.4.1 on Windows or JRE 1.4.2 on Linux to test
 public class cryto
 {
   public static void main(String[] args)
